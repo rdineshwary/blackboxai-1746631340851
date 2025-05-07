751215f58e0fb751d9545340111591491de38763
rdineshwary <rdinehwary@gmail.com>
2025-05-07 23:22:44 +0800

    Add sdckl-library-management-system/backend/migrations/20230610-create-user.js 1746631363119

---
+++ sdckl-library-management-system/backend/migrations/20230610-create-user.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      studentId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      studentName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      passwordHash: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.ENUM('admin', 'member'),
+        defaultValue: 'member'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
